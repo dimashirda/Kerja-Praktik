f4a070bc5c7f2f840b0e7aaa0f9d62546e2605e2
ranitera <raniaulia72@gmail.com>
2017-07-18 14:28:26 +0700

    account mgr add edit delete

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,12 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+//Route::resource('acc-mgr', 'AccountManagerController');
+
+Route::get('acc-mgr', 'AccountManagerController@index');
+Route::get('acc-mgr/create', 'AccountManagerController@create');
+Route::post('acc-mgr/store', 'AccountManagerController@store');
+Route::get('acc-mgr/edit/{id}', 'AccountManagerController@edit');
+Route::post('acc-mgr/update', 'AccountManagerController@update');
+Route::get('acc-mgr/delete/{id}', 'AccountManagerController@delete');
