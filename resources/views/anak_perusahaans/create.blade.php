d0830a4f8b2eb44c4babab0f40cb92d13e78b4c3
MSI\Rian <riandanisap@gmail.com>
2017-07-18 14:01:44 +0700

    anak perusahaan update

---
+++ resources/views/anak_perusahaans/create.blade.php
@@ -0,0 +1,22 @@
+<div class="input-field col s12">
+                    <i class="material-icons prefix">person</i>
+                    <input id="nama" type="text" class="validate" name="nama" value="" required>
+                    <label for="icon_prefix">ID_Perusahaan</label>
+                </div>
+
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">email</i>
+                    <input id="email" type="text" class="validate" name="email" value="" required>
+                    <label for="icon_prefix">Nama_perusahaan</label>
+                </div>
+
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">phone</i>
+                    <input id="telepon" type="text" class="validate" name="telepon" value="" required>
+                    <label for="icon_prefix">Telepon_Perusahaan</label>
+                </div>
+                <div class="input-field col s12">
+                    <i class="material-icons prefix">phone</i>
+                    <input id="telepon" type="text" class="validate" name="telepon" value="" required>
+                    <label for="icon_prefix">Email_perusahaan</label>
+                </div>
---
+++ resources/views/anak_perusahaans/index.blade.php
@@ -0,0 +1,22 @@
+@if($acc->count())
+<div class="table-responsive">
+<table class="table table-bordered table-striped table-hover table-condensed tfix">
+	<thead align="center"><tr>
+	<td><b>ID_perusahaan</b></td>
+	<td><b>nama_perusahaan</b></td>
+	<td><b>tlp_perusahaan</b></td>
+	<td><b>email_perusahaan</b></td>
+	</tr></thead>
+@foreach($acc as $a)
+<tr>
+<td>{{ $a->id_perusahaan }}</td>
+<td>{{ $a->nama_perusahaan }}</td>
+<td>{{ $a->tlp_perusahaan }}</td>
+<td>{{ $a->email_perusahaan }}</td>
+</tr>
+@endforeach
+</table></div>
+@else
+<div class="alert alert-warning">
+<i class=fa fa-exclamation-triangle"></i> DATA TIDAK DITEMUKAN</div>
+@endif
