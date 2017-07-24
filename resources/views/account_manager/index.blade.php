3483f635da9e73acd61fb508cc4320ccd30476e3
ranitera <raniaulia72@gmail.com>
2017-07-24 10:37:07 +0700

    nambah search

--- resources/views/account_manager/index.blade.php
+++ resources/views/account_manager/index.blade.php
@@ -1,28 +1,193 @@
-@if($acc->count())
-<div class="table-responsive">
-<table class="table table-bordered table-striped table-hover table-condensed tfix">
- <thead align="center"> <tr> 
- <td><b>ID Acc Manager</b></td> 
- <td><b>Nama Acc Manager</b></td> 
- <td><b>Tlp Acc Manager</b></td>
- <td><b>Email Acc Manager</b></td>
- <td colspan="2"><b>MENU</b></td>
- </tr></thead>
-@foreach($acc as $a)
- <tr>
- 	<td>{{ $a->id_am }}</td>
- 	<td>{{ $a->nama_am }}</td>
- 	<td>{{ $a->tlp_am }}</td>
- 	<td>{{ $a->email_am }}</td>
- 	<td>
- 		<a href="acc-mgr/edit/{{$a->id_am}}">edit</a>
- 		<a href="acc-mgr/delete/{{$a->id_am}}">hapus</a>
- 	</td>
- </tr>
-@endforeach
-</table>
-</div>
-@else
- <div class="alert alert-warning">
- <i class="fa fa-exclamation-triangle"></i> Data Acc Mgr tidak ditemukan</div>
-@endif
+@extends('adminlte::page')
+
+@section('title', 'SIKontrak')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+
+
+@section('content')
+    <style>
+        .example-modal .modal {
+            position: relative;
+            top: auto;
+            bottom: auto;
+            right: auto;
+            left: auto;
+            display: block;
+            z-index: 1;
+        }
+
+        .example-modal .modal {
+            background: transparent !important;
+        }
+    </style>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box">
+                <div class="box-header">
+                    <h3 class="box-title">Account Manager</h3>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                    <form action="{{url('admin/accmgr')}}" method="get" role="search">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div id="example1_filter" class="form-inline">
+                                    <div class="form-group">
+                                        <label>Search by:
+                                            <select name="kategori" class="form-control input-sm">
+                                                <option value="nama">Nama Account Manager</option>
+                                                <option value="ID">ID Account Manager</option>
+                                            </select>
+                                            <input type="search" class="form-control input-sm" name="search" placeholder aria-controls="example1">
+                                            <button type="submit" class="btn btn-info btn-flat input-sm">Search</button>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                    <br>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a href="{{route('addaccmgr')}}" class='btn btn-primary'><i class="fa fa-plus-circle"></i> Tambah baru</a>
+                        </div>
+                    </div>
+                    <br>
+                    @if($acc->count())
+                    <table class="table table-bordered table-hover">
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nama</th>
+                            <th>No. Telepon</th>
+                            <th>Email</th>
+                            <th colspan="2">Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($acc as $a)
+                        <tr>
+                            <td>{{ $a->id_am }}</td>
+                            <td>{{ $a->nama_am }}</td>
+                            <td>{{ $a->tlp_am }}</td>
+                            <td>{{ $a->email_am }}</td>
+                            <td align="center" width="30px">
+                                <button type="button" class="btn btn-default edit-button" data-toggle="modal" data-target="#modal-default" data-id="{{$a->id_am}}" data-name="{{$a->nama_am}}" data-telp="{{$a->tlp_am}}" data-email="{{$a->email_am}}">
+                                    Edit
+                                </button>
+                            </td>
+                            <td align="center" width="30px">
+                                <button type="button" class="btn btn-danger delete-button" data-name="{{$a->nama_am}}" data-id="{{$a->id_am}}" data-toggle="modal" data-target="#modal-danger">
+                                    Hapus
+                                </button>
+                            </td>
+                        </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                        {{$acc->render()}}
+                </div>
+                <div id="modal-default" class="modal fade" style="display: none;">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span></button>
+                                <h4 class="modal-title">Edit Data Account Manager</h4>
+                            </div>
+                            <div class="modal-body">
+                                <form class="form-horizontal" method="post" id="form-edit">
+                                    {{csrf_field()}}
+                                    <div class="box-body">
+                                        <div class="form-group">
+                                            <label for="IDAccMgr" class="col-sm-2 control-label">ID Account Manager</label>
+
+                                            <div class="col-sm-10">
+                                                <input type="text" class="form-control" name="id_accm" id="idaccmgr" disabled>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="NamaAccMgr" class="col-sm-2 control-label">Nama Account Manager</label>
+
+                                            <div class="col-sm-10">
+                                                <input type="text" class="form-control" name="nama_accm" id="namaaccmgr">
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="TlpAccMgr" class="col-sm-2 control-label">No. Telepon</label>
+
+                                            <div class="col-sm-10">
+                                                <input type="text" class="form-control" name="tlp_accm" id="tlpaccmgr">
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="EmailAccMgr" class="col-sm-2 control-label">Email</label>
+
+                                            <div class="col-sm-10">
+                                                <input type="email" class="form-control" name="email_accm" id="emailaccmgr">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+                                        <button type="submit" class="btn btn-success pull-right">Simpan</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="modal-danger" class="modal fade" style="display: none;">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span></button>
+                                <h4 class="modal-title">Hapus Data</h4>
+                            </div>
+                            <div class="modal-body">
+                                <p id="show-name"></p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+                                <a id="del-btn">
+                                    <button type="button" class="btn btn-success pull-right">Hapus</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @else
+                    <p>Data tidak ditemukan</p>
+                @endif
+            </div>
+        </div>
+    </div>
+    <script>
+        $(document).on("click", ".edit-button", function(){
+            var id_accmgr = $(this).data('id');
+            var nama_accmgr = $(this).data('name');
+            var email_accmgr = $(this).data('email');
+            var tlp_accmgr = $(this).data('telp');
+            $("#idaccmgr").val(id_accmgr);
+            $("#namaaccmgr").val(nama_accmgr);
+            $("#emailaccmgr").val(email_accmgr);
+            $("#tlpaccmgr").val(tlp_accmgr);
+
+            $("#form-edit").attr('action','{{url('/admin/accmgr/edit')}}' + '/' + id_accmgr);
+        })
+
+        $(document).on("click",".delete-button", function () {
+            var id_accmgr = $(this).data('id');
+            var nama_accmgr = $(this).data('name');
+            $("#del-btn").attr('href','{{url('admin/accmgr/delete')}}' + '/' + id_accmgr);
+            $("#show-name").html('Anda yakin ingin menghapus Account Manager ' + nama_accmgr + '?')
+
+        })
+    </script>
+@stop
+
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,20 +9,55 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
-
-//Route::resource('acc-mgr', 'AccountManagerController');
-
-Route::get('acc-mgr', 'AccountManagerController@index');
-Route::get('acc-mgr/create', 'AccountManagerController@create');
-Route::post('acc-mgr/store', 'AccountManagerController@store');
-Route::get('acc-mgr/edit/{id}', 'AccountManagerController@edit');
-Route::post('acc-mgr/update', 'AccountManagerController@update');
-Route::get('acc-mgr/delete/{id}', 'AccountManagerController@delete');
+Route::get('/admin', 'AdminControllers@index')->name('admin');
+Route::get('/admin/upload', 'DetilKontrakController@index')->name('upload');
+Route::get('/admin/pelanggan', 'PelangganController@index')->name('pelanggan');
+Route::get('/admin/pelanggan/create', 'PelangganController@create')->name('addplg');
+Route::get('/admin/perusahaan', 'AnakPerusahaanController@index')->name('perusahaan');
+Route::get('/admin/perusahaan/create', 'AnakPerusahaanController@create')->name('addprshn');
+Route::get('/admin/layanan', 'LayananController@index')->name('layanan');
+Route::get('/admin/layanan/create', 'LayananController@create')->name('addlyn');
+Route::get('/admin/accmgr', 'AccountManagerController@index')->name('accmgr');
+Route::get('/admin/accmgr/create', 'AccountManagerController@create')->name('addaccmgr');
+Route::get('/upload', 'DetilKontrakController@index')->name('upload');
+//Route::get('acc-mgr', 'AccountManagerController@index');
+//Route::get('acc-mgr/create', 'AccountManagerController@create');
+Route::post('admin/accmgr/store', 'AccountManagerController@store');
+//Route::get('admin/accmgr/edit/{id}', 'AccountManagerController@edit');
+Route::post('admin/accmgr/edit/{id}', 'AccountManagerController@update');
+Route::get('admin/accmgr/delete/{id}', 'AccountManagerController@delete');
+Route::post('admin/pelanggan/store','PelangganController@store');
+Route::post('admin/pelanggan/edit/{nipnas}','PelangganController@save');
+Route::get('pelanggan/create','PelangganController@create');
+//Route::get('admin/pelanggan/edit}','PelangganController@edit');
+Route::get('admin/pelanggan/delete/{nipnas}','PelangganController@delete');
+//Route::get('pelanggan','PelangganController@index');
+Route::post('tambahkontrak','DetilKontrakController@store');
+Route::post('editkontrak','DetilKontrakController@save');
+Route::get('kontrak/create','DetilKontrakController@create');
+Route::get('kontrak/edit/{id_detil}','DetilKontrakController@edit');
+Route::get('kontrak/delete/{id_detil}','DetilKontrakController@delete');
+Route::get('kontrak','DetilKontrakController@index');
+Route::get('kontrak/download/{nama_dokumen}','DetilKontrakController@download');
+Route::get('searchkontrak','DetilKontrakController@search');
+Route::post('admin/layanan/create','LayananController@store');
+Route::post('editlayanan','LayananController@save');
+//Route::get('admin/layanan/create','LayananController@create');
+Route::get('layanan/edit/{id}','LayananController@edit');
+Route::get('admin/layanan/delete/{id}','LayananController@delete');
+Route::get('admin/layanan','LayananController@index');
+Route::post('admin/perusahaan/store','AnakPerusahaanController@store');
+Route::post('admin/perusahaan/edit/{id_perusahaan}','AnakPerusahaanController@save');
+Route::get('anak_perusahaans/create','AnakPerusahaanController@create');
+//Route::get('admin/perusahaan/edit/{id_perusahaan}','AnakPerusahaanController@edit');
+Route::get('admin/perusahaan/delete/{id_perusahaan}','AnakPerusahaanController@delete');
+Route::get('anak_perusahaans','AnakPerusahaanController@index');
+Route::get('upload', function() {
+  return View::make('anak_perusahaans.upload');
+});
+Route::post('apply/upload', 'ApplyController@upload');
