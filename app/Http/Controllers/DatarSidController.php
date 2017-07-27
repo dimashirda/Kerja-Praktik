c443d8b7909aa8cedd637a60db738caed53de1b8
ranitera <raniaulia72@gmail.com>
2017-07-27 14:22:17 +0700

    nambah tabel layanan_imes & daftar_sid

---
+++ app/Http/Controllers/DatarSidController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Datar_sid;
+use Illuminate\Http\Request;
+
+class DatarSidController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Datar_sid  $datar_sid
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Datar_sid $datar_sid)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Datar_sid  $datar_sid
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Datar_sid $datar_sid)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Datar_sid  $datar_sid
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Datar_sid $datar_sid)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Datar_sid  $datar_sid
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Datar_sid $datar_sid)
+    {
+        //
+    }
+}
